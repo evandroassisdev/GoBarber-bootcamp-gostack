511d64d2ca4f83dc84442df4570d64b25b25c446
Evandro Assis <evandroassisdev@gmail.com>
2019-08-29 16:52:30 -0400

    Filas de cancelamento de horarios

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '8e8c4e9fb1c6ea',
+    pass: '970cd6987d74d1',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
